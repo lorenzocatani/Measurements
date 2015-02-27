1ec458763558d2cc83731a68252dd023d9f5a606
Lorenzo Catani <lorenzo.catani4@gmail.com>
2015-02-27 11:52:11 +0000

    add function and adjust measures

--- unit.py
+++ unit.py
@@ -4,11 +4,11 @@ I have to create class to implement operations consistently with the units
 # we define a class that takes  the value (e.g. 5) and the unit (e.g. km). It then creates the measure (length or time).
 class Measure(object):
         def __init__(self,value,unit):
-         self.value=value
-         self.unit=unit
-		 if unit=='km' or unit=='m':
+                 self.value=value
+                 self.unit=unit
+		 if self.unit=='km' or self.unit=='m':
 		    return self.measure='length'
-		 if unit=='h' or unit=='s':
+		 if self.unit=='h' or self.unit=='s':
 		    return self.measure='time'
 		# this function convert the current unit into the one indicated by unitconvertion.	
 		def conversion(self, unitconversion):
@@ -20,12 +20,12 @@ class Measure(object):
              return 60*self.value
          elif self.unit=='s' and unitconversion=='h':
              return 0.016666666666666666666666666666667*self.value
-		# this function add the values of self and others if they are the same unit or if they are the same measure.		 
+		# this function adds the values of self and others if they are the same unit or if they are the same measure.		 
         def add(self, others):
              if self.measure==others.measure:
                  if self.unit==others.unit:
                      return self.value+others.value
                  else:
-                     return others.value+self.conversion(others.unit)     
+                     return self.value+others.conversion(self.unit)     
              else:
                  raise ValueError('Units not summable') 				 
